683ff66624d412e1fec4f4bc43646ffbd1666ee8
Mohsin Ali <mohsinalisep@gmail.com>
2024-11-01 02:51:57 +0500

    Added compnents - navbar

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Navbar from './navbar/Navbar';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const names = ['mario', 'luigi', 'yoshi'];
+  const google_url = 'https://www.google.com';
+  const book = {
+    title: 'Harry Potter',
+    author: 'JK Rowling',
+    pages: 394
+  }
   return (
     <div className="App">
-      <div className="content">
-        <h1>Hello World</h1>
-      </div>
+      <Navbar/>
+      {/* <div className="content">
+        <h1>{ title }</h1>
+        <h2>{ [1, 2, 3, 4, 5] }</h2>
+        <h2>{ Math.random() * 10 }</h2>
+        <a href={ google_url }>Google</a>
+      </div> */}
     </div>
   );
 }
---
+++ blog/src/navbar/Navbar.js
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>The Blog</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/create">Blog</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
